7616908d4e72691d32d9484cb956df7670007323
Aaron Turner <Turneraaron97@gmail.com>
2023-05-22 17:08:00 -0400

    Finished connection page

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
-const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const connection = require('./db/connection');
 
-const PORT = process.env.PORT || 3001;
+require('console.table');
+
+function introPrompt(){
+inquirer.prompt([
+    {
+        type: 'list',
+        name: 'menu',
+        message: 'Select your choice',
+        choices: [
+           'View Employees',
+           'View Departments',
+           'View Roles',
+           'Add an Employee',
+           'Create a Role',
+           'Create a Department',
+           'Employee Role modification',
+        ]
+    }
+]).then((res) => {
+    console.log(res.menu);
+    switch(res.menu){
+        case 'View Employees':
+        viewEmployees();
+        break;
+        case 'View Departments':
+            viewDepartments();
+        break;
+        case 'View Roles':
+                viewRoles();
+        break;
+        case 'Add an Employee':
+            addEmployee();
+         break;
+         case 'Create a Role':
+            createRole();
+        break;
+        case 'Create a Department':
+            createDepartment();
+        break;
+        case 'Employee Role Modification':
+            employeeRole();
+        break;
+    }
+}).catch((err) => {
+    if(err) throw err;
+})
+}
